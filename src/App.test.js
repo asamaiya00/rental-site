595e5c922cfb2020d63ca436e9e825b20fd8480b
Animesh Samaiya <asamaiya00@gmail.com>
2022-04-02 22:41:45 +0530

    testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
 import App from './App';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+test('header is rendered', () => {
+  const wrapper = shallow(<App />);
+  const headerEl = wrapper.find('header');
+  expect(headerEl.length).toBe(1);
 });
